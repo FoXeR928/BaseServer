1d40fccf15360683911742232673f90deb26d729
FoXeR <57914341+FoXeR928@users.noreply.github.com>
2021-05-24 10:23:52 +0300

    Добавленны комментарии

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import fastapi
 from loguru import logger
 
+# Путь записи логов
 logger.add("logs/info.log", format="{time} {level} {message}")
+# Инициализация fastapi
 app = fastapi.FastAPI()
 
 try:
-
+    # Создание страницы
     @app.get("/info")
     def root():
         return {"message": "Привет"}
--- config.py
+++ config.py
@@ -1,19 +1,23 @@
 import configparser
 from loguru import logger
 
+# Путь записи логов
 logger.add("logs/info.log", format="{time} {level} {message}")
 try:
+    # Инициализация configparser
     config = configparser.ConfigParser()
+    # Получение данных из config.init
     config.read("config.ini")
     logger.info(f'Host: {config["host"]["ip"]} and Port {config["host"]["port"]}')
 except:
     logger.error("Host and Port not take")
 
-
+# Класс получения тегов
 class Base:
     def __init__(self, ip, port):
         self.ip = ip
         self.port = port
 
 
+# Введение переменных в класс
 all = Base(config["host"]["ip"], int(config["host"]["port"]))
