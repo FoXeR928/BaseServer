2f4ee334a2f429fd0c6964e4fad302a532d65606
FoXeR <57914341+FoXeR928@users.noreply.github.com>
2021-05-24 14:22:26 +0300

    Добавлена страница пользоаателя, через config.init

--- app.py
+++ app.py
@@ -1,21 +1,30 @@
 import fastapi
 import logs
 from loguru import logger
+from config import load_config
 
 # Добавлени параметров логов
 logs.init_log()
 # Инициализация fastapi
 app = fastapi.FastAPI()
-
-try:
-
+user=load_config()
     # Запуск страницы info
-    @app.get("/info")
-    def root():
+@app.get("/info")
+def root():
+    try:
         return {"message": "Привет"}
-
-    # Сообщение о работе страницы
-    logger.info("Page info work")
-except Exception as err:
-    # Сообщение о ошибке страницы
-    logger.error(f"Server not work. ERROR: {err}")
+        # Сообщение о работе страницы
+        logger.info("Page info work")
+    except Exception as err:
+        # Сообщение о ошибке страницы
+        logger.error(f"Server not work. ERROR: {err}")
+    
+@app.get(f"/{user.name}")
+def root():
+    try:
+        return {"message": f"Привет {user.name}"}
+        # Сообщение о работе страницы
+        logger.info("Page info work")
+    except Exception as err:
+        # Сообщение о ошибке страницы
+        logger.error(f"Server not work. ERROR: {err}")
--- config.py
+++ config.py
@@ -1,24 +1,23 @@
 import configparser
+from os import name
 import logs
 from loguru import logger
 
 logs.init_log()
 
-
+try:
+    # Получение парараметров из config.ini
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+    # Сообщение о получение конфигов
+    logger.info(f'Host: {config["host"]["ip"]} and Port: {config["host"]["port"]}')
+except Exception as err:
+    # Сообщение о ошибке получения конфигов
+    logger.error(f"Server not work. ERROR: {err}")
 def load_config():
-    try:
-        # Получение парараметров из config.ini
-        config = configparser.ConfigParser()
-        config.read("config.ini")
-        # Сообщение о получение конфигов
-        logger.info(f'Host: {config["host"]["ip"]} and Port: {config["host"]["port"]}')
-    except Exception as err:
-        # Сообщение о ошибке получения конфигов
-        logger.error(f"Server not work. ERROR: {err}")
-    return Base(config["host"]["ip"], int(config["host"]["port"]))
-
-
+    return Base(config["host"]["ip"], int(config["host"]["port"]), config["users"]["user1"])
 class Base:
-    def __init__(self, ip, port):
+    def __init__(self, ip, port, name):
         self.ip = ip
         self.port = port
+        self.name=name
