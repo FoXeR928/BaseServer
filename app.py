e65aff271613ea7b5c867d586c25e2aa4273c5b9
FoXeR <57914341+FoXeR928@users.noreply.github.com>
2021-05-26 15:08:48 +0300

    Изменён подход к получению данных из бд

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 import fastapi
 import logs
-import sql
 from loguru import logger
 from config import load_config
+import sql
 
 # Добавлени параметров логов
 logs.init_log()
 # Инициализация fastapi
 app = fastapi.FastAPI()
 user = load_config()
+
+def value(surename):
+    print (surename)
 
 @app.on_event("startup")
 def start():
@@ -30,24 +33,24 @@ def root():
 
 
 @app.get("/user_get/")
-def get(name: str = fastapi.Query(None)):
-    return name
-  
-for x in sql.base_check():
+def get_user(name: str, surename: str, patronymic: str):
+    return name, surename, patronymic
 
-    @app.get(f"/user/{x[0]}")
-    def name():
-        try:
-            # Сообщение о работе страницы
-            logger.info(f"Page user/{x[0]} work")  
-            message={"message": f"Привет {x[1]}"}
-            return message
-        except Exception as err:
-            # Сообщение о ошибке страницы
-            logger.error(f"Server not work. ERROR: {err}")
-            message=str(err)
-            return fastapi.Response(content=message, status_code=500)
+@app.get("/user/{surename}")
+def name(surename: str):
+    value(surename)
+    name=sql.base_check()
+    try:
+        # Сообщение о работе страницы
+        logger.info(f"Page user/{surename} work")  
+        message={"message": f"Привет {name}"}
+        return message
+    except Exception as err:
+        # Сообщение о ошибке страницы
+        logger.error(f"Server not work. ERROR: {err}")
+        message=str(err)
+        return fastapi.Response(content=message, status_code=500)
 
-    @app.on_event("shutdown")
-    def shutdown():
-        logger.info("Server stop work")
+@app.on_event("shutdown")
+def shutdown():
+    logger.info("Server stop work")
--- sql.py
+++ sql.py
@@ -1,22 +1,29 @@
 from logging import info
-from os import name
 import sqlite3
 import logs
 from loguru import logger
+import app
 
 logs.init_log()
+
 
 try:
     connect_sql= sqlite3.connect('bd.db')
     curs=connect_sql.cursor()
     logger.debug('Base connect')
-except:
-    logger.error('Base not connect')
+except sqlite3.Error and Exception as err:
+    logger.error('Base not work. ERROR: {err}')
 
 try:
+    def base_right():
+        curs.execute("""INSERT INTO tabl2(name, surename, patronymic) VALUES ('iPhone X', 'Apple', 5);""")
+        return curs.fetchall()
+except sqlite3.Error and Exception as err:
+    logger.error('Base not work. ERROR: {err}')
+try:
     def base_check():
-        curs.execute("""SELECT id_name, name from testBD""")
-        return(curs.fetchall())
+        curs.execute(""f"SELECT name, surename, patronymic from testBD WHERE surename='{app.value()}'""")
+        return curs.fetchall()
     logger.info("Base date take")
 except sqlite3.Error and Exception as err:
     logger.error(f"Base not work. ERROR: {err}")
