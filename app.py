b2c520e5f7633508ce17ec31718553dc982c83c6
FoXeR <57914341+FoXeR928@users.noreply.github.com>
2021-05-27 13:23:11 +0300

    Внесены измененя

--- app.py
+++ app.py
@@ -32,36 +32,36 @@ def root():
     return message
 
 
-@app.post("/user_post/")
-def get_user(name: str, surename: str, patronymic: str):
+@app.post("/user_post/", status_code=fastapi.status.HTTP_201_CREATED)
+def post_user(name: str, surename: str, patronymic: str, code: fastapi.Response):
     try:
-        result = sql.base_recording(name, surename, patronymic)
+        message = sql.base_recording(name, surename, patronymic)
     except Exception as err:
         message = str(err)
         logger.error(f"Server not work. ERROR: {err}")
-        result = fastapi.Response(content=message, status_code=500)
-    return result
+        code.status_code = fastapi.status.HTTP_501_NOT_IMPLEMENTED
+    return message
 
 
-@app.get("/user/{surename}")
-def name(surename: str):
+@app.get("/user/{surename}", status_code=fastapi.status.HTTP_200_OK)
+def name(surename: str, code: fastapi.Response):
     try:
         name = sql.base_check(surename)
     except Exception as err:
         message = str(err)
         logger.error(f"Server not work. ERROR: {err}")
-        result = fastapi.Response(content=message, status_code=404)
+        code.status_code = fastapi.status.HTTP_404_NOT_FOUND
     try:
         # Сообщение о работе страницы
         logger.info(f"Page user/{surename} work")
         for name in name:
-            result = {"message": f"Привет {name[0]} {name[1]} {name[2]}"}
+            message = {"message": f"Привет {name[0]} {name[1]} {name[2]}"}
     except Exception as err:
         # Сообщение о ошибке страницы
         logger.error(f"Server not work. ERROR: {err}")
         message = str(err)
-        result = fastapi.Response(content=message, status_code=500)
-    return result
+        code.status_code = fastapi.status.HTTP_500_INTERNAL_SERVER_ERROR
+    return message
 
 
 @app.on_event("shutdown")
