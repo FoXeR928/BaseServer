d9d83d4b895ac240a02f78456e1df81e35647e21
FoXeR <57914341+FoXeR928@users.noreply.github.com>
2021-05-27 10:53:51 +0300

    Добавленно сохранение в базу

--- sql.py
+++ sql.py
@@ -1,7 +1,6 @@
 import sqlite3
 import logs
 from loguru import logger
-import app
 
 logs.init_log()
 
@@ -9,18 +8,19 @@ def base_recording(name, surename, patronymic):
     try:
         connect_sql= sqlite3.connect('bd.db')
         curs=connect_sql.cursor()
-        curs.execute(""f"INSERT INTO testBD(name, surename, patronymic) VALUES ('{name}', '{surename}', '{patronymic}');""")
+        curs.execute(f"INSERT INTO testBD(name, surename, patronymic) VALUES ('{name}', '{surename}', '{patronymic}');")
         logger.debug(f'Base recording. {name, surename, patronymic}')
-        return curs.fetchall()
     except sqlite3.Error and Exception as err:
         logger.error(f'Base recording. ERROR: {err}')
+    connect_sql.commit()
 
 def base_check(surename):
     try:
-        connect_sql= sqlite3.connect('bd.db')
+        connect_sql= sqlite3.connect('bd.db', timeout=5)
         curs=connect_sql.cursor()
         curs.execute(f"SELECT name, surename, patronymic from testBD WHERE surename='{surename}'")
         logger.info("Base date take")
+        
         return curs.fetchall()
     except sqlite3.Error and Exception as err:
         logger.error(f"Base not take. ERROR: {err}")
