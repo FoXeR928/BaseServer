12d4b5fbdfd39a68e81735e638a629e0f55ed2d2
FoXeR <57914341+FoXeR928@users.noreply.github.com>
2021-06-10 15:52:18 +0300

    Возвращены тесты app

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,14 @@
 import mimesis
+import sqlite3
 import sys
 import pytest
+
 from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
 sys.path.append("./")
 import app
-from db_set import Base, Tabl
-import config
+from config import base, tabl
+from sql import open_base
 
 test_client = TestClient(app.app)
 
@@ -20,79 +20,27 @@ code_400 = 400
 code_404 = 404
 code_422 = 422
 
-
 def open_base(base):
-    sys.path.append("../")
-    engine = create_engine(f"sqlite:///{base}.db")
-    Base.metadata.bind = engine
-    DBSession = sessionmaker(bind=engine)
-    session = DBSession()
-    open_base.engine = engine
-    return session
+    connect_sql = sqlite3.connect(f"{base}.db", timeout=5)
+    curs = connect_sql.cursor()
+    open_base.connect_sql = connect_sql
+    return curs
 
 
 @pytest.yield_fixture(autouse=True)
 def base_create():
-    session = open_base(config.base)
-    session.query(Tabl).delete()
-    session.commit()
-    record = Tabl(
-        device_id="name_one",
-        device_path="text_txt",
-        device_reg="text_reg",
-        date_in="2011-10-13 16:23:16.083572",
+    curs = open_base(base)
+    curs.execute(f"DELETE FROM {tabl}")
+    curs.execute(
+        f"""INSERT INTO {tabl}(device_id, device_path, device_reg, date_in, date_out, fio, tabnum, department)
+                    VALUES ('name_one','text_txt','text_reg', '2011-10-13 16:23:16.083572',NULL,NULL,NULL,NULL),
+                    ('name2','text_txt','text_reg','2011-10-13 16:23:16.083572','2019-03-07 23:17:50.848051','Кетрин Чимоканова',359254064417561,'Режиссер'),
+                    ('name3','text_txt','text_reg','2011-10-13 16:23:16.083572','2019-03-07 23:17:50.848051',NULL,NULL,NULL),
+                    ('name4','text_txt','text_reg','2011-10-13 16:23:16.083572','2019-03-07 23:17:50.848051','Велигор Миссюров',353166055808564,'Травматолог'),
+                    ('name5','text_txt','text_reg','2011-10-13 16:23:16.083572','2019-03-07 23:17:50.848051','Хосе Подюков',329304008876062,'Психиатр'),
+                    ('name6','text_txt','text_reg','2011-10-13 16:23:16.083572','2019-03-07 23:17:50.848051','Ынтымак Горляков',358240054017520,'Кассир');"""
     )
-    record2 = Tabl(
-        device_id="name2",
-        device_path="text_txt",
-        device_reg="text_reg",
-        date_in="2011-10-13 16:23:16.083572",
-        date_out="2019-03-07 23:17:50.848051",
-        fio="Кетрин Чимоканова",
-        tabnum=359254064417561,
-        department="Режиссер",
-    )
-    record3 = Tabl(
-        device_id="name3",
-        device_path="text_txt",
-        device_reg="text_reg",
-        date_in="2011-10-13 16:23:16.083572",
-        date_out="2019-03-07 23:17:50.848051",
-    )
-    record4 = Tabl(
-        device_id="name4",
-        device_path="text_txt",
-        device_reg="text_reg",
-        date_in="2011-10-13 16:23:16.083572",
-        date_out="2019-03-07 23:17:50.848051",
-        fio="Велигор Миссюров",
-        tabnum=353166055808564,
-        department="Травматолог",
-    )
-    record5 = Tabl(
-        device_id="name5",
-        device_path="text_txt",
-        device_reg="text_reg",
-        date_in="2011-10-13 16:23:16.083572",
-        date_out="2019-03-07 23:17:50.848051",
-        fio="Хосе Подюков",
-        tabnum=329304008876062,
-        department="Психиатр",
-    )
-    record6 = Tabl(
-        device_id="name6",
-        device_path="text_txt",
-        device_reg="text_reg",
-        date_in="2011-10-13 16:23:16.083572",
-        date_out="2019-03-07 23:17:50.848051",
-        fio="Ынтымак Горляков",
-        tabnum=358240054017520,
-        department="Кассир",
-    )
-    session.add_all([record, record2, record3, record4, record5, record6])
-    session.commit()
-    session.close()
-
+    open_base.connect_sql.commit()
     yield
 
 
@@ -116,19 +64,18 @@ def test_true_upload_file():
     responses = test_client.post("/upload_file", files=files)
     assert responses.status_code == code_201
     assert responses.json() == ["Added to base"]
-    responses = test_client.get("/date_flask/?device_id=P1601450070867E90D1B6300")
-    assert responses.status_code == code_200
-    assert responses.json() == {
-        "Flask": [
-            "Здесь сообщение, которое проверяет правильность "
-            "записи в базу, но сообщение я не придумал поэтому "
-            "psoskgepgodfle[porypoyietor[glhkrpoyitrpyglkfg;hnmf]]",
-            "А тут написан другой текст для проверки "
-            "работоспособности записи в базу и текст этот из "
-            "себя представляет "
-            "p[rtier0934-0534lkjfldfgt3048po;egkdhdlgypeortyiglmmdbr454k]",
-        ],
-    }
+    curs = open_base(base)
+    curs.execute(
+        f"SELECT device_id, device_path, device_reg FROM {tabl} WHERE device_id='P1601450070867E90D1B6300'"
+    )
+    result = curs.fetchall()
+    assert result == [
+        (
+            "P1601450070867E90D1B6300",
+            "Здесь сообщение, которое проверяет правильность записи в базу, но сообщение я не придумал поэтому psoskgepgodfle[porypoyietor[glhkrpoyitrpyglkfg;hnmf]]",
+            "А тут написан другой текст для проверки работоспособности записи в базу и текст этот из себя представляет p[rtier0934-0534lkjfldfgt3048po;egkdhdlgypeortyiglmmdbr454k]",
+        )
+    ]
 
 
 def test_true_2_upload_file():
@@ -154,10 +101,7 @@ def test_false_upload_file():
     ]
     responses = test_client.post("/upload_file", files=files)
     assert responses.status_code == code_422
-    assert responses.json() == [
-        """ERROR: (sqlite3.IntegrityError) UNIQUE constraint failed: tabl2.device_id\n[SQL: INSERT INTO tabl2 (device_id, device_path, device_reg, date_in, date_out, fio, tabnum, department) VALUES (?, ?, ?, ?, 
-?, ?, ?, ?)]\n[parameters: ('name_one', 'ответ', 'ответ', datetime.datetime(2021, 6, 10, 10, 0, 11, 541568), None, None, None, None)]\n(Background on this error at: http://sqlalche.me/e/14/gkpj)"""
-    ]
+    assert responses.json() == ["ERROR: UNIQUE constraint failed: tabl2.device_id"]
 
 
 def test_false_2_upload_file():
@@ -196,31 +140,6 @@ def test_upload_file():
     assert responses.json() == ["Incorrect file name"]
 
 
-def test_true_search_device_id():
-    responses = test_client.get("/id_flask?device_id=name_one")
-    assert responses.status_code == code_200
-    assert responses.json() == {
-        "Flask": [
-            [
-                "name_one",
-                "text_txt",
-                "text_reg",
-                "2011-10-13 16:23:16.083572",
-                None,
-                None,
-                None,
-                None,
-            ]
-        ],
-    }
-
-
-def test_false_search_device_id():
-    responses = test_client.get("/id_flask?device_id=1")
-    assert responses.status_code == code_404
-    assert responses.json() == ["Not result"]
-
-
 def test_device_id_date():
     responses = test_client.get("/date_flask/?device_id=1")
     assert responses.status_code == code_404
@@ -242,7 +161,7 @@ def test_give_file():
         f"/give_flask?device_id={device_id}&fio={fio}&tabnum={tabnum}&department={department}"
     )
     assert responses.status_code == code_422
-    assert responses.json() == ["ERROR: No row was found when one was required"]
+    assert responses.json() == ['ERROR: No row was found when one was required']
 
 
 def test_true_give_file():
@@ -256,7 +175,7 @@ def test_true_give_file():
 def test_device_id_get():
     responses = test_client.put("/get_flask?device_id=1")
     assert responses.status_code == code_422
-    assert responses.json() == ["ERROR: No row was found when one was required"]
+    assert responses.json() == ['ERROR: No row was found when one was required']
 
 
 def test_true_device_id_get():
@@ -376,14 +295,6 @@ def test_true_name_flask():
             ]
         ]
     }
-
-
-def test_false_tabnum_flask():
-    responses = test_client.get(
-        "/tabnum_flask?tabnum=3333333333333333333333333333333333"
-    )
-    assert responses.status_code == code_404
-    assert responses.json() == ["Not result"]
 
 
 def test_true_tabnum_flask():
